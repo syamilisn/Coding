2805a819531e7aeb7e1c18b55005011e0b0fef6b
syamilisn <shyamilisn.itfa@gmail.com>
2023-02-24 08:25:22 +0530

    02/24/23

--- A3_Kernels/B1_Modules/.c
+++ A3_Kernels/B1_Modules/.c
@@ -1,7 +1,7 @@
 /*
-* Date       : 23-02-2023
-* Version    : #58~20.04.1-Ubuntu SMP Thu Oct 13 13:09:46 UTC 2022
-* Release    : 5.15.0-52-generic
+* Date       : 24-02-2023
+* Version    : #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2
+* Release    : 5.19.0-32-generic
 *
 * Modulename : 
 * Description: 
