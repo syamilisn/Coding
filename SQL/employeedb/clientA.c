6cfad3754c5c64b59afa9181e562371be5f95a4a
syamilisn <shyamilisn.itfa@gmail.com>
2023-05-05 13:55:30 +0530

    05/05/23

--- SQL/employeedb/clientA.c
+++ SQL/employeedb/clientA.c
@@ -20,19 +20,19 @@ struct mesg_buffer{
     long mesg_type;
     int success;
     struct employee emp;
-};
+}msg;
 
-struct mesg_buffer msg = {
-    .mesg_type = 1,
-    .success = 0,
-    .emp = {
-        .id = 1,
-        .name = "shyam",
-        .domain = "kdd",
-        .lang = "c",
-        .expc = 1
-    }
-};
+// struct mesg_buffer msg = {
+//     .mesg_type = 1,
+//     .success = -1,
+//     .emp = {
+//         .id = 1,
+//         .name = "shyam",
+//         .domain = "kdd",
+//         .lang = "c",
+//         .expc = 1
+//     }
+// };
 
 key_t key;
 int msgid;
@@ -46,15 +46,46 @@ char *password = "Cuppie@123";
 char *database = "employeedb";
 
 int emp_exists(int send_id){
-    return 1;
+    string table_name = "employee";
+    query fetch;
+    sprintf(fetch, "SELECT * FROM %s WHERE id = %d", table_name, send_id);
+    int exists;
+    if(mysql_query(conn, fetch)){
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+    res = mysql_use_result(conn);   // line required for fetching connection results even if not result are not required
+    row = mysql_fetch_row(res);
+    
+    if(row == NULL)
+        exists = 0;
+    else
+        exists = 1;
+    mysql_free_result(res);
+    return exists;
 }
 
 void fetch_emp(int send_id){
-    
+    string table_name = "employee";
+    query fetch;
+    sprintf(fetch, "SELECT * FROM %s WHERE id = %d", table_name, send_id);
+    if(mysql_query(conn, fetch)){
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+    res = mysql_use_result(conn);
+    row = mysql_fetch_row(res);
+    msg.emp.id = send_id;
+    strcpy(msg.emp.name, row[1]);
+    strcpy(msg.emp.domain, row[2]);
+    strcpy(msg.emp.lang, row[3]);
+    msg.emp.expc = atoi(row[4]);
+    mysql_free_result(res);
 }
 
 int main(int argc, char* argv[]){
-
+    int flag = 1;
+    string choice;
     // Initialize connection object
     conn = mysql_init(NULL);
 
@@ -63,39 +94,47 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "%s\n", mysql_error(conn));
         exit(1);
     }
+    do{
+        key = ftok("progfile", 65);
+        msgid = msgget(key, 0666 | IPC_CREAT);
 
-    key = ftok("progfile", 65);
-    msgid = msgget(key, 0666 | IPC_CREAT);
-
-    // Enter emp_id
-    int send_id;
-    printf("Enter employee id to be sent: \n");
-    scanf("%d", &send_id);
-    if(emp_exists(send_id)){
-        //  send mechanism
-        msg.mesg_type = 1;
-            //fetch data from DB here >>
-        fetch_emp(send_id);
-        msgsnd(msgid, &msg, sizeof(msg), 0);
-        printf("Message sent: id-%d name-%s domain-%s lang-%s expc-%d \n", msg.emp.id, msg.emp.name, msg.emp.domain, msg.emp.lang, msg.emp.expc);
-        msgrcv(msgid, &msg, sizeof(msg), 2, 0);
-        printf("Message received: id-%d name-%s domain-%s lang-%s expc-%d \n", msg.emp.id, msg.emp.name, msg.emp.domain, msg.emp.lang, msg.emp.expc);
-        if(msg.success == 0)
-            //  if message is received and read successfully by client B
-            printf("ACK = %d ...success \n", msg.success);
+        // Enter emp_id
+        int send_id;
+        printf("Enter employee id to be sent: \n");
+        scanf("%d", &send_id);
+        if(emp_exists(send_id) == 1){
+            printf("Employee exists... \n");
+            //  send mechanism
+            msg.mesg_type = 1;
+                //fetch data from DB here >>
+            fetch_emp(send_id);
+            msgsnd(msgid, &msg, sizeof(msg), 0);
+            printf("Message sent: id-%d name-%s domain-%s lang-%s expc-%d \n", msg.emp.id, msg.emp.name, msg.emp.domain, msg.emp.lang, msg.emp.expc);
+            msgrcv(msgid, &msg, sizeof(msg), 2, 0);
+            printf("Message received: id-%d name-%s domain-%s lang-%s expc-%d \n", msg.emp.id, msg.emp.name, msg.emp.domain, msg.emp.lang, msg.emp.expc);
+            if(msg.success == 0)
+                //  if message is received and read successfully by client B
+                printf("ACK = %d ...success \n", msg.success);
+            else{
+                //  if message NOT received and read successfully by client B
+                printf("NACK = %d ...failure \n", msg.success);
+                exit(0);
+                // break;
+            }
+            
+        }  
+        
         else{
-            //  if message NOT received and read successfully by client B
-            printf("NACK = %d ...failure \n", msg.success);
+            printf("Employee with id = %d doesn't exist in employee database. \n", send_id);
             exit(0);
-            // break;
         }
-
-        msgctl(msgid, IPC_RMID, NULL);
-    }
-    else{
-        printf("Employee with id = %d doesn't exist in employee database. \n", send_id);
-    }
-
+        msgctl(msgid, IPC_RMID, NULL);   
+        printf("Continue? y/n: ");
+        scanf("%s", choice);
+        printf(" \n");
+        if(strcmp(choice, "n")==0 || strcmp(choice, "N")==0)
+            break;
+    }while(flag);
     mysql_close(conn);
     return 0;
 }
--- SQL/employeedb/clientB.c
+++ SQL/employeedb/clientB.c
@@ -42,10 +42,41 @@ char *password = "Cuppie@123";
 char *database = "employeedb";
 
 int emp_exists(int receive_id){
-    return 1;
+    string table_name = "employee2";
+    query fetch;
+    sprintf(fetch, "SELECT * FROM %s WHERE id = %d", table_name, receive_id);
+    int exists;
+    if(mysql_query(conn, fetch)){
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+    res = mysql_use_result(conn);   // line required for fetching connection results even if not result are not required
+    row = mysql_fetch_row(res);
+    
+    if(row == NULL)
+        exists = 0;
+    else
+        exists = 1;
+    mysql_free_result(res);
+    return exists;
 }
 
 void fetch_emp(int receive_id){
+    string table_name = "employee2";
+    query fetch;
+    sprintf(fetch, "SELECT * FROM %s WHERE id = %d", table_name, receive_id);
+    if(mysql_query(conn, fetch)){
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+    res = mysql_use_result(conn);
+    row = mysql_fetch_row(res);
+    myemp.id = receive_id;
+    strcpy(myemp.name, row[1]);
+    strcpy(myemp.domain, row[2]);
+    strcpy(myemp.lang, row[3]);
+    myemp.expc = atoi(row[4]);
+    mysql_free_result(res);
 
 }
 
@@ -59,55 +90,62 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "%s\n", mysql_error(conn));
         exit(1);
     }
+    while(1){
+        key = ftok("progfile", 65);
+        msgid = msgget(key, 0666 | IPC_CREAT);
 
-    key = ftok("progfile", 65);
-    msgid = msgget(key, 0666 | IPC_CREAT);
 
-    int receive_id = 1;     // obtained from mesg_buf
-    if(emp_exists(receive_id)){
         //  receive mechanism
         msgrcv(msgid, &msg, sizeof(msg), 1, 0);
+        int receive_id = msg.emp.id;     // obtained from mesg_buf
+        printf("Employee id received: %d \n", receive_id);
         printf("Message received: id-%d name-%s domain-%s lang-%s expc-%d \n", msg.emp.id, msg.emp.name, msg.emp.domain, msg.emp.lang, msg.emp.expc);
-        //  fetch from database
-        fetch_emp(receive_id);
-        printf("Database details: id-%d name-%s domain-%s lang-%s expc-%d \n", myemp.id, myemp.name, myemp.domain, myemp.lang, myemp.expc);
-        msg.mesg_type = 2;
-        msg.success = 0;        //  message received success
 
-        if(strcmp(msg.emp.name, myemp.name) != 0){
-            printf("name mismatched! \n");
-            msg.success = -1;    //  message name not matched with DB
+        if(emp_exists(receive_id) == 1){
+            //  fetch from database
+            fetch_emp(receive_id);
+            printf("Database details: id-%d name-%s domain-%s lang-%s expc-%d \n", myemp.id, myemp.name, myemp.domain, myemp.lang, myemp.expc);
+            msg.mesg_type = 2;
+            msg.success = 0;        //  message received success
+
+            if(strcmp(msg.emp.name, myemp.name) != 0){
+                printf("\tname mismatched! \n");
+                msg.success = -1;    //  message name not matched with DB
+            }
+            if(strcmp(msg.emp.domain, myemp.domain) != 0){
+                printf("\tdomain mismatched! \n");
+                msg.success = -1;    //  message domain not matched with DB
+            }
+            if(strcmp(msg.emp.lang, myemp.lang) != 0){
+                printf("\tlanguage mismatched! \n");
+                msg.success = -1;    //  message lang not matched with DB
+            }
+            if(msg.emp.expc != myemp.expc){
+                printf("\texperience mismatched! \n");
+                msg.success = -1;    //  message experience not matched with DB
+            }
+                //fetch data from DB here >>
+            msgsnd(msgid, &msg, sizeof(msg), 0);
+            printf("Message sent: id-%d name-%s domain-%s lang-%s expc-%d \n", msg.emp.id, msg.emp.name, msg.emp.domain, msg.emp.lang, msg.emp.expc);
+            if(msg.success == 0)
+                //  if message is received and read successfully by client B
+                printf("ACK = %d ...success \n", msg.success);
+            else{
+                //  if message NOT received and read successfully by client B
+                printf("NACK = %d ...failure \n", msg.success);
+                exit(0);
+                // break;
+            }
         }
-        if(strcmp(msg.emp.domain, myemp.domain) != 0){
-            printf("domain mismatched! \n");
-            msg.success = -1;    //  message domain not matched with DB
-        }
-        if(strcmp(msg.emp.lang, myemp.lang) != 0){
-            printf("language mismatched! \n");
-            msg.success = -1;    //  message lang not matched with DB
-        }
-        if(msg.emp.expc != myemp.expc){
-            printf("experience mismatched! \n");
-            msg.success = -1;    //  message experience not matched with DB
-        }
-            //fetch data from DB here >>
-        msgsnd(msgid, &msg, sizeof(msg), 0);
-        printf("Message sent: id-%d name-%s domain-%s lang-%s expc-%d \n", msg.emp.id, msg.emp.name, msg.emp.domain, msg.emp.lang, msg.emp.expc);
-        if(msg.success == 0)
-            //  if message is received and read successfully by client B
-            printf("ACK = %d ...success \n", msg.success);
         else{
-            //  if message NOT received and read successfully by client B
-            printf("NACK = %d ...failure \n", msg.success);
-            exit(0);
-            // break;
+            printf("Employee with id = %d doesn't exist in employee2 database. \n", receive_id);
+            // msg.success = -1;
+            // printf("NACK = %d ...failure \n", msg.success);
+            // exit(0);
         }
         msgctl(msgid, IPC_RMID, NULL);
+        printf("************************************************************\n");
     }
-    else{
-        printf("Employee with id = %d doesn't exist in employee2 database. \n", receive_id);
-    }
-
     mysql_close(conn);
     return 0;
 }
